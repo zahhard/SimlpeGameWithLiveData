9c29b48ced9efdd29e88484c67f3a3d36f896795
zahhard <za.darardoust@gmail.com>
2022-04-09 23:36:52 +0430

    I add 6 more question to repository and all of the answers

--- app/src/main/java/com/example/gamelivedata/QuestionRepository.kt
+++ app/src/main/java/com/example/gamelivedata/QuestionRepository.kt
@@ -8,5 +8,25 @@ object QuestionRepository {
         "result of : 48 + 17 ",
         "result of : 51 - 42 ",
         "result of : 98 * 89 ",
+        "result of : 12 + 44 ",
+        "result of : 199 - 58",
+        "result of : 32 * 13 ",
+        "result of : 153 + 17 ",
+        "result of : 352 - 42 ",
+        "result of : 98 * 9 ",
+    )
+    val answerList = arrayListOf(
+        "92",
+        "13",
+        "132",
+        "65",
+        "9",
+        "8722",
+        "56",
+        "141",
+        "45",
+        "170",
+        "314",
+        "882",
     )
 }
