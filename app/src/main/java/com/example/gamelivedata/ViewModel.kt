a4aba700ef04e3babe97f2e97ffd39d3fb52f7e2
zahhard <za.darardoust@gmail.com>
2022-04-09 23:37:58 +0430

    init function added , checkCorrectly function added, ScoreAnalyzeLiveData Transformation added,

--- app/src/main/java/com/example/gamelivedata/ViewModel.kt
+++ app/src/main/java/com/example/gamelivedata/ViewModel.kt
@@ -6,16 +6,24 @@ import androidx.lifecycle.ViewModel
 class ViewModel : ViewModel(){
 
     val questionCount = QuestionRepository.questionList.size
-    val numberLiveData = MutableLiveData<Int>(0)
+    val numberLiveData = MutableLiveData<Int>(1)
     val nextEnableLiveData = MutableLiveData<Boolean>(true)
     val backEnableLiveData = MutableLiveData<Boolean>(false)
     val questionTextLiveData = MutableLiveData<String>()
-    val scoreLiveData = MutableLiveData<Int>()
+    val answerTextLiveData = MutableLiveData<String>()
+    val scoreLiveData = MutableLiveData<Int>(0)
+
+    fun init(){
+        numberLiveData.value?.let {
+            questionTextLiveData.value = QuestionRepository.questionList[it - 1]
+        }
+    }
 
     fun nextClicked() {
         numberLiveData.value = numberLiveData.value?.plus(1)
         numberLiveData.value?.let {
-            questionTextLiveData.value = QuestionRepository.questionList[it-1]
+            questionTextLiveData.value = QuestionRepository.questionList[it - 1]
+            answerTextLiveData.value = QuestionRepository.answerList[it - 2]
         }
         checkEnabled()
     }
@@ -34,14 +42,34 @@ class ViewModel : ViewModel(){
         numberLiveData.value = numberLiveData.value?.minus(1)
         numberLiveData.value?.let {
             questionTextLiveData.value = QuestionRepository.questionList[it-1]
+            answerTextLiveData.value = QuestionRepository.answerList[it-2]
         }
         checkEnabled()
     }
 
-    val hintLiveData = Transformations.map( numberLiveData) {
+    fun checkCorrectly(text: String) {
+        if (answerTextLiveData.value.toString() == text){
+            scoreLiveData.value = scoreLiveData.value?.plus(3)
+        }
+        else
+            scoreLiveData.value = scoreLiveData.value?.minus(1)
+        scoreLiveData
+    }
+
+    val hintLiveData = Transformations.map( numberLiveData ) {
         if (it <= questionCount / 2)
             "Hurry up"
         else
             "good"
+    }
+
+    val ScoreAnalyzeLiveData = Transformations.map( scoreLiveData ) {
+        if (it <= questionCount / 4)
+            "#f4171f"
+        else if (it > questionCount / 4 && it <= questionCount / 2)
+            "#ff7f27"
+        else
+            "#0ed145"
+
     }
 }
