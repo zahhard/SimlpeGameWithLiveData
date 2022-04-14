f174e3448944d6f3608859f1f0c4988f51f7d344
zahhard <za.darardoust@gmail.com>
2022-04-14 10:43:38 +0430

    addTestData fun , newRandomQuestion fun, addNewQuestion fun

--- app/src/main/java/com/example/gamelivedata/QuestionRepository.kt
+++ app/src/main/java/com/example/gamelivedata/QuestionRepository.kt
@@ -1,9 +1,10 @@
 package com.example.gamelivedata
 
 import android.content.Context
+import kotlin.random.Random
 
 object QuestionRepository {
-    val questionList = arrayListOf(
+   val questionList = arrayListOf(
         "result of : 25 + 67 ",
         "result of : 15 - 2 ",
         "result of : 77 * 55 ",
@@ -32,9 +33,48 @@ object QuestionRepository {
         "882",
     )
 
+    var id = 2
+
     var db : AppDatabase? = null
     var questionDao : QuestionDao? = null
     fun initDB(context: Context){
         db = AppDatabase.getDatabase(context)
+        questionDao = db?.questionDao()
+        addTestData()
+    }
+
+    private fun addTestData() {
+        questionDao?.insertAll(
+            Question(0, "result of : 25 + 67 ", 92),
+            Question(1,  "result of : 15 - 2 ", 13),
+            newRandomQuestion(),
+            newRandomQuestion(),
+            newRandomQuestion(),
+            newRandomQuestion()
+        )
+
+    }
+
+    private fun newRandomQuestion(): Question{
+        id ++
+        var firstNum = Random.nextInt()
+        var secondNum = Random.nextInt()
+        var answer = 0
+        var desc = ""
+         if (firstNum <= secondNum) {
+            answer = secondNum - firstNum
+             desc = "result of : $secondNum - $firstNum"
+        } else {
+           answer = firstNum - secondNum
+             desc = "result of : $firstNum - $secondNum"
+        }
+
+        return Question(id, desc, answer)
+    }
+
+    fun addNewQuestion(){
+        db!!.questionDao().insertAll(
+            newRandomQuestion()
+        )
     }
 }
