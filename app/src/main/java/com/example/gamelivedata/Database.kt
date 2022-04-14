4a152d84fb42f9641d47d63747a600d381f1cb1e
zahhard <za.darardoust@gmail.com>
2022-04-14 09:58:58 +0430

    Database class updated and an abstract fun and a companien object added

--- app/src/main/java/com/example/gamelivedata/Database.kt
+++ app/src/main/java/com/example/gamelivedata/Database.kt
@@ -1,6 +1,32 @@
 package com.example.gamelivedata
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
+@Database(entities = [Question::class], version = 1)
+abstract class AppDatabase : RoomDatabase(){
+    abstract fun questionDao(): QuestionDao
 
-class Database {
+    companion object{
+        var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase? {
+            if (INSTANCE == null){
+                synchronized(AppDatabase::class){
+                    INSTANCE =
+                        Room.databaseBuilder(context.applicationContext,
+                            AppDatabase::class.java, "myDB")
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyDataBase(){
+            INSTANCE = null
+        }
+    }
 }
